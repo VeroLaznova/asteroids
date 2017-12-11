8a1a11d8fd0499b3fe97174c3db9e021092ac504
Veronika Laznova <laznova.veronika@gmail.com>
2017-12-11 18:58:22 +0100

    spaceship loaded

--- game.py
+++ game.py
@@ -5,11 +5,31 @@ import pyglet
 objects = []
 
 # Clases:
-...
+class Spaceship:
+    def __init__ (self):
+        self.x = 0
+        self.y = 0
+        self.rotation = 0
+        self.x_speed = 0
+        self.y_speed = 0
+        image = pyglet.image.load('spaceship.png')
+        self.sprite = pyglet.sprite.Sprite(image)
+        objects.append(self)
+
+spaceship = Spaceship()
 
 
 
 
 # Pyglet
 window = pyglet.window.Window()
+
+def draw_all_objects():
+    window.clear()
+    for obj in objects:
+        obj.sprite.draw()
+
+window.push_handlers(
+    on_draw = draw_all_objects,
+)
 pyglet.app.run()
