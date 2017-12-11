841beed75f7bb470799e35b6db55e5fdb6987f88
Veronika Laznova <laznova.veronika@gmail.com>
2017-12-11 19:20:45 +0100

    adding picture, spaceship moving

--- game.py
+++ game.py
@@ -10,8 +10,8 @@ class Spaceship:
         self.x = 20
         self.y = 40
         self.rotation = 0
-        self.x_speed = 0
-        self.y_speed = 0
+        self.x_speed = 1
+        self.y_speed = 3
 
         image = pyglet.image.load('spaceship.png')
         self.sprite = pyglet.sprite.Sprite(image)
@@ -22,6 +22,12 @@ class Spaceship:
     def update_sprite(self):
         self.sprite.x = self.x
         self.sprite.y = self.y
+
+    def tick(self,dt):
+        self.x += dt*self.x_speed
+        self.x += dt*self.y_speed
+        self.update_sprite()
+
 
 
 spaceship = Spaceship()
@@ -37,7 +43,14 @@ def draw_all_objects():
     for obj in objects:
         obj.sprite.draw()
 
+def tick_all_objects(dt):
+    for obj in objects:
+        obj.tick(dt)
+
 window.push_handlers(
     on_draw = draw_all_objects,
 )
+
+pyglet.clock.schedule_interval(tick_all_objects, 1/30)
+
 pyglet.app.run()
